edba6802f4b6afbfa2b0e048d88497cc2cd45817
Colin Zheng <zbinlin@gmail.com>
2016-11-19 19:34:38 +0800

    {FIX}: fix firefox install error: `it appears to be corrupt`

--- generate-manifest.js
+++ generate-manifest.js
@@ -6,6 +6,13 @@ const pkg = require("./package.json");
 const source = require("./src/manifest.json");
 
 source.version = pkg.version;
+if (/^firefox$/i.test(process.env.BUILD_TARGET)) {
+    source.applications = {
+        "gecko": {
+            "id": "something@mozilla.org",
+        },
+    };
+}
 
 const dest = JSON.stringify(source, null, "    ");
 fs.writeFileSync(path.join(process.cwd(), "./dist/manifest.json"), dest);
